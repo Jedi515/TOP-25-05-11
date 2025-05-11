ba6c06b85df3826d34dc4acd7586216686da1706
Jedi515 <jedimailpost515@mail.ru>
2025-05-11 20:22:05 +1000

    Update HelloWorld.cs

--- Assets/TutorialInfo/Scripts/HelloWorld.cs
+++ Assets/TutorialInfo/Scripts/HelloWorld.cs
@@ -12,14 +12,12 @@ public class HelloWorld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < 0 || transform.position.y > 10f)
+        if (transform.position.y < 0 || transform.position.y > 5f)
         {
             direction *= -1;
         }
 
-
         transform.Translate(0, 0.01f * direction, 0);
         transform.Rotate(0, 90/60f, 0);
-        
     }
 }
