d4940e9b68d5c21de90dbfbd0fb6f6dadcd08711
Jedi515 <jedimailpost515@mail.ru>
2025-05-11 20:19:38 +1000

    Goes up and down

---
+++ Assets/TutorialInfo/Scripts/HelloWorld.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world!");
+    }
+    float direction = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < 0 || transform.position.y > 10f)
+        {
+            direction *= -1;
+        }
+
+
+        transform.Translate(0, 0.01f * direction, 0);
+        transform.Rotate(0, 90/60f, 0);
+        
+    }
+}
